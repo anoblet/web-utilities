d06a77ad4d03bc1bda6b1b191bd036956cc3dc17
Andrew Noblet <andrewbnoblet@gmail.com>
2021-01-04 09:09:52 -0500

    chore(package): save

--- lib/ServiceWorker.d.ts
+++ lib/ServiceWorker.d.ts
@@ -1,6 +1,19 @@
+export declare const install: ({ source }: {
+    source: string;
+}) => void;
 export declare const registerServiceWorker: ({ installed, message, source, }: {
     installed?: (event: any) => any;
     message?: (event: any) => any;
     source: string;
 }) => void;
 export declare const showUpdateSnackbar: () => Promise<void>;
+export declare class ServiceWorker {
+    private installedCallback;
+    private local;
+    private messageCallback;
+    private source;
+    constructor(properties: {
+        source: string;
+    });
+    register(): void;
+}
--- src/ServiceWorker.ts
+++ src/ServiceWorker.ts
@@ -1,6 +1,27 @@
 import { html } from "lit-element";
 import { render } from "lit-html";
 
+export const install = ({ source }: { source: string }) => {
+  if (location.hostname !== "localhost") {
+    let updateRequested = false;
+    registerServiceWorker({
+      installed: (event) => {
+        if (event.isUpdate) {
+          if (!updateRequested) showUpdateSnackbar();
+          updateRequested = true;
+        }
+      },
+      message: (event) => {
+        if (event.data.meta === "workbox-broadcast-update") {
+          if (!updateRequested) showUpdateSnackbar();
+          updateRequested = true;
+        }
+      },
+      source,
+    });
+  }
+};
+
 export const registerServiceWorker = ({
   installed,
   message,
@@ -40,3 +61,57 @@ const reload = () => {
   window.location.reload();
   return false;
 };
+
+const snackbar = async () => {
+  await import("@material/mwc-snackbar");
+  await import("@material/mwc-button");
+
+  const snackbar = html`
+    <mwc-snackbar labelText="Application has been updated">
+      <mwc-button slot="action" @click=${reload}>Reload</mwc-button>
+    </mwc-snackbar>
+  `;
+
+  const el = document.createElement("div");
+  document.body.appendChild(el);
+  render(snackbar, el);
+  el.querySelector("mwc-snackbar").open = true;
+};
+
+export class ServiceWorker {
+  private installedCallback: () => void = snackbar;
+  private local: boolean = false;
+  private messageCallback: () => void = snackbar;
+  private source: string;
+
+  constructor(properties: { source: string }) {
+    Object.assign(this, properties);
+
+    this.register();
+  }
+
+  register() {
+    // Make sure we are not running locally
+    if (!this.local && location.hostname === "localhost") return;
+
+    // Make sure the browser supports a service worker
+    if (!("serviceWorker" in navigator)) return;
+
+    import("workbox-window").then(({ Workbox }) => {
+      const workbox = new Workbox(this.source);
+      this.installedCallback &&
+        workbox.addEventListener(
+          "installed",
+          (event) => event.isUpdate && this.installedCallback()
+        );
+      this.messageCallback &&
+        workbox.addEventListener(
+          "message",
+          (event) =>
+            event.data.meta === "workbox-broadcast-update" &&
+            this.messageCallback()
+        );
+      workbox.register();
+    });
+  }
+}
